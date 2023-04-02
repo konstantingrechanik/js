ddca5f09d9ede99c9e5f06ef8f5c6ab2cd1d0f1f
konstantingrechanik <118936809+konstantingrechanik@users.noreply.github.com>
2023-04-02 20:47:55 +0200

    js code

---
+++ script.js
@@ -0,0 +1,42 @@
+//const pClass = document.getElementsByClassName("test");
+//pClass[0].innerText = "Hello!";
+
+//let cl = document.getElementsByClassName("test");
+//console.log(cl);
+//for( let i = 0; i < cl.length; i++){
+//    cl[i].innerText = "Hello";
+//}
+
+//const listOfLi = document.getElementsByClassName("text");
+//for ( iet i = 0; i < listOfLi.length; i++){
+//  listOfLi[i].innerText = i;
+//}
+
+//selector
+// const special_li = document.querySelectorAll("li.special");
+
+
+
+//const image = document.querySelectorAll(img);
+//image.setAttribute(
+//    "src","https://i.pinimg.com/originals/18/4b/8f/184b8f2c10e30326f6ee92b3652408ac.jpg"
+    
+//const link = document.querySelectorAll("a");
+//let newLink = [];
+//for(key in link){  
+//    newLink.push(link[key].textContent);
+//}
+//console.log
+
+
+
+/*const links = ["https://www.google.com/",
+"https://www.freepik.com/",
+"https://www.pexels.com/"
+] 
+const arrLink = document.querySelectorAll("a");
+for(let i =0; i < arrLink.length; i++){
+    arrLink[i].setAttribute("href",links[i]);
+}
+console.log(arrLink);
+*/
